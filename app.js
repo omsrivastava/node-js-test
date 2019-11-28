ef6d930866d50acfb8616674b3b7f5fd2f93a819
Om Prakash Srivastava <OmPrakash.Srivastava@ingrammicro.com>
2019-11-28 19:47:47 +0530

    Handle POST requests

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
+
 app.get('/', function(req, res) {
-  res.send('This is the home page!');
+  res.render('index');
 });
 
 app.get('/contact', function(req, res) {
-  res.send('This is the Contact page');
+  res.render('contact', { qr: req.query });
 });
 
-app.get('/profile/:id', function(req, res) {
-  res.send('You requested to see the profile with id: ' + req.params.id);
+app.post('/contact', urlencodedParser, function(req, res) {
+  res.render('contact-success', { data: req.body });
+});
+
+app.get('/profile/:name', function(req, res) {
+  const data = { age: 26, job: 'Developer', hobbies: ['Coding', 'Eating', 'Gaming'] };
+
+  res.render('profile', { person: req.params.name, data });
 });
 
 app.listen(4200);
